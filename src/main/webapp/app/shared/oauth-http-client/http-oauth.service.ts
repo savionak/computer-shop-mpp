9d38dfdfffba76119d7349e4105a3cb2b060e7da
LizaSandul <lizasandul97@mail.ru>
2017-04-27 23:41:18 +0300

    Simplify <HttpOAuthService>

--- src/main/webapp/app/shared/oauth-http-client/http-oauth.service.ts
+++ src/main/webapp/app/shared/oauth-http-client/http-oauth.service.ts
@@ -3,26 +3,10 @@ import {Http, RequestOptionsArgs, Response} from "@angular/http";
 
 import {Observable} from "rxjs/Observable";
 
-// TODO: handle OAuth2 to api requests
+
 @Injectable()
-export class HttpOAuthService {
-    constructor(@Inject(Http) private http: Http) {
+export class HttpOAuthService extends Http {
 
-    }
+    // handle OAuth2 to api requests
 
-    public get(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        return this.http.get(url, options);
-    }
-
-    public post(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
-        return this.http.post(url, body, options);
-    }
-
-    public put(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
-        return this.http.put(url, body, options);
-    }
-
-    public delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        return this.http.delete(url, options);
-    }
 }
