9681d5db4d68eea6b305869f7c62b4b13cd25339
LizaSandul <lizasandul97@mail.ru>
2017-04-28 00:43:21 +0300

    Fix bug with injection

--- src/main/webapp/app/shared/oauth-http-client/http-oauth.service.ts
+++ src/main/webapp/app/shared/oauth-http-client/http-oauth.service.ts
@@ -1,12 +1,44 @@
 import {Inject, Injectable} from "@angular/core";
-import {Http, RequestOptionsArgs, Response} from "@angular/http";
+import {Http, RequestOptionsArgs, Request, Response} from "@angular/http";
 
 import {Observable} from "rxjs/Observable";
 
-
+// TODO: handle OAuth2 to api requests
 @Injectable()
-export class HttpOAuthService extends Http {
+export class HttpOAuthService {
+    constructor(@Inject(Http) private http: Http) {
 
-    // handle OAuth2 to api requests
+    }
 
+    request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.request(url, options);
+    };
+
+    get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.get(url, options);
+    }
+
+    post(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.post(url, body, options);
+    }
+
+    put(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.put(url, body, options);
+    }
+
+    delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.delete(url, options);
+    }
+
+    patch(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.patch(url, body, options);
+    };
+
+    head(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.head(url, options);
+    };
+
+    options(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return this.http.options(url, options);
+    };
 }
