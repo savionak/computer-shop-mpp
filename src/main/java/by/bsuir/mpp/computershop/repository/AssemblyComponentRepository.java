9bd7002d15d686eede9783398ed77834adc21f92
Vladislav Savionok <savionak@gmail.com>
2017-05-01 02:02:46 +0300

    [Crud] Make repositories PagingAndSorting

--- src/main/java/by/bsuir/mpp/computershop/repository/AssemblyComponentRepository.java
+++ src/main/java/by/bsuir/mpp/computershop/repository/AssemblyComponentRepository.java
@@ -1,7 +1,7 @@
 package by.bsuir.mpp.computershop.repository;
 
 import by.bsuir.mpp.computershop.entity.AssemblyComponent;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AssemblyComponentRepository extends CrudRepository<AssemblyComponent, Long> {
+public interface AssemblyComponentRepository extends PagingAndSortingRepository<AssemblyComponent, Long> {
 }
--- src/main/java/by/bsuir/mpp/computershop/repository/OrderRepository.java
+++ src/main/java/by/bsuir/mpp/computershop/repository/OrderRepository.java
@@ -1,7 +1,7 @@
 package by.bsuir.mpp.computershop.repository;
 
 import by.bsuir.mpp.computershop.entity.Order;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
 }
