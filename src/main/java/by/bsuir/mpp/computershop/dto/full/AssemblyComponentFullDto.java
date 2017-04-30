3ca20ab8e8bda1b1b8de03ddbc10d476ad1c06dc
Vladislav Savionok <savionak@gmail.com>
2017-04-30 04:36:24 +0300

    [Spring] Implement AssemblyComponentFullDto
    + clean entity

---
+++ src/main/java/by/bsuir/mpp/computershop/dto/full/AssemblyComponentFullDto.java
@@ -0,0 +1,36 @@
+package by.bsuir.mpp.computershop.dto.full;
+
+import by.bsuir.mpp.computershop.dto.brief.BaseBriefDto;
+import by.bsuir.mpp.computershop.dto.brief.ComponentStoreBriefDto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import static by.bsuir.mpp.computershop.utils.ValidationConstants.CANNOT_BE_NEGATIVE_MESSAGE;
+import static by.bsuir.mpp.computershop.utils.ValidationConstants.CANNOT_BE_NULL_MESSAGE;
+
+public class AssemblyComponentFullDto extends BaseBriefDto {
+
+    @NotNull(message = CANNOT_BE_NULL_MESSAGE)
+    private ComponentStoreBriefDto component;
+
+    @NotNull(message = CANNOT_BE_NULL_MESSAGE)
+    @Min(value = 0, message = CANNOT_BE_NEGATIVE_MESSAGE)
+    private Long count;
+
+    public ComponentStoreBriefDto getComponent() {
+        return component;
+    }
+
+    public void setComponent(ComponentStoreBriefDto component) {
+        this.component = component;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+}
--- src/main/java/by/bsuir/mpp/computershop/entity/AssemblyComponent.java
+++ src/main/java/by/bsuir/mpp/computershop/entity/AssemblyComponent.java
@@ -1,32 +1,21 @@
 package by.bsuir.mpp.computershop.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-
-import static by.bsuir.mpp.computershop.utils.ValidationConstants.CANNOT_BE_NEGATIVE_MESSAGE;
-import static by.bsuir.mpp.computershop.utils.ValidationConstants.CANNOT_BE_NULL_MESSAGE;
 
 @Entity
 @Table(name = "assembly_component")
 public class AssemblyComponent extends BaseEntity<Long> implements Serializable {
-    @JsonIgnore
     @Id
     @ManyToOne
     @JoinColumn(name = "assembly_id", nullable = false)
     private Assembly assembly;
 
-    @NotNull(message = CANNOT_BE_NULL_MESSAGE)
     @Id
     @ManyToOne
     @JoinColumn(name = "component_id", nullable = false)
     private ComponentStore component;
 
-    @NotNull(message = CANNOT_BE_NULL_MESSAGE)
-    @Min(value = 0, message = CANNOT_BE_NEGATIVE_MESSAGE)
     @Column(name = "count", nullable = false)
     private Long count;
 
