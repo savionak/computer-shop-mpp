74b65d76c898b2bda62daf235c193066f3924e11
Vladislav Savionok <savionak@gmail.com>
2017-05-01 00:51:16 +0300

    [Test] Update primary test class

--- src/test/java/by/bsuir/mpp/computershop/ComputerShopMppApplicationTests.java
+++ src/test/java/by/bsuir/mpp/computershop/ComputerShopMppApplicationTests.java
@@ -1,16 +1,19 @@
 package by.bsuir.mpp.computershop;
 
+import by.bsuir.mpp.computershop.config.PrimaryConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {PrimaryConfiguration.class})
 @SpringBootTest
 public class ComputerShopMppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
