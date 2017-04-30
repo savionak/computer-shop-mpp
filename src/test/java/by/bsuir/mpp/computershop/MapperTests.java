e0416add6b70e4d764fc5691741939cab89ea0b8
Vladislav Savionok <savionak@gmail.com>
2017-05-01 00:02:41 +0300

    [Test] Update MapperTests
    * add userAuthConverterTest

--- src/test/java/by/bsuir/mpp/computershop/MapperTests.java
+++ src/test/java/by/bsuir/mpp/computershop/MapperTests.java
@@ -2,37 +2,36 @@ package by.bsuir.mpp.computershop;
 
 import by.bsuir.mpp.computershop.config.PrimaryConfiguration;
 import by.bsuir.mpp.computershop.dto.full.AssemblyComponentFullDto;
+import by.bsuir.mpp.computershop.dto.full.UserAuthFullDto;
 import by.bsuir.mpp.computershop.entity.Assembly;
 import by.bsuir.mpp.computershop.entity.AssemblyComponent;
+import by.bsuir.mpp.computershop.entity.UserAuth;
+import by.bsuir.mpp.computershop.utils.TestHelper;
 import ma.glasnost.orika.MapperFacade;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {PrimaryConfiguration.class})
 @SpringBootTest
 public class MapperTests {
 
-    private final Random rnd = new Random();
-
     @Autowired
     private MapperFacade mapper;
 
-    @Test
-    public void contextLoads() {
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     public void assemblyComponentMappingTest() {
         Assembly asm = new Assembly();
-        asm.setId(getNextId());
+        asm.setId(TestHelper.nextLongId());
 
         AssemblyComponent expected = new AssemblyComponent();
         expected.setAssembly(asm);
@@ -41,11 +40,21 @@ public class MapperTests {
         Assert.assertEquals(expected.getAssembly().getId(), intermediate.getAssemblyId());
 
         AssemblyComponent result = mapper.map(intermediate, AssemblyComponent.class);
+
         Assert.assertEquals(expected.getId(), result.getId());
         Assert.assertEquals(expected.getAssembly().getId(), result.getAssembly().getId());
     }
 
-    private Long getNextId() {
-        return rnd.nextLong() + 1;
+    @Test
+    public void userAuthConverterTest() {
+        UserAuthFullDto sourceAuthDto = TestHelper.nextUserAuthFullDto();
+
+        UserAuth entity = mapper.map(sourceAuthDto, UserAuth.class);
+
+        Assert.assertEquals(entity.getEmail(), sourceAuthDto.getEmail());
+        Assert.assertEquals(entity.getRole(), sourceAuthDto.getRole());
+        Assert.assertEquals(entity.getUserInfo().getFirstName(), sourceAuthDto.getUserInfo().getFirstName());
+
+        Assert.assertTrue(passwordEncoder.matches(sourceAuthDto.getPass(), entity.getPassHash()));
     }
 }
