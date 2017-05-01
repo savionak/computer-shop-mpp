b361cd6c9b6059c05c0b06ab6703000667fa6002
LizaSandul <lizasandul97@mail.ru>
2017-05-01 03:05:02 +0300

    Add header/footer of page

---
+++ src/main/webapp/app/header.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'header',
+    template: `
+<div class="header">
+    <span class="header_title"> Computer Store    </span>
+    </div>`
+})
+export class HeaderComponent { }
