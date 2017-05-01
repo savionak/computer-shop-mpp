1ec9c74a748febc2d7d21a0de3f5dfcc9ca981d7
Vladislav Savionok <savionak@gmail.com>
2017-05-01 16:50:48 +0300

    [Crud] Update PageDto
    * add PageInfo class

--- src/main/java/by/bsuir/mpp/computershop/dto/brief/pages/PageDto.java
+++ src/main/java/by/bsuir/mpp/computershop/dto/brief/pages/PageDto.java
@@ -2,57 +2,9 @@ package by.bsuir.mpp.computershop.dto.brief.pages;
 
 public class PageDto {
 
-    private int totalPages;
-
-    private int number;
-
-    private int size;
-
-    private long totalElements;
-
-    private int numberOfElements;   // <= content.length
-
     private Iterable content;
 
-    public int getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public long getTotalElements() {
-        return totalElements;
-    }
-
-    public void setTotalElements(long totalElements) {
-        this.totalElements = totalElements;
-    }
-
-    public int getNumberOfElements() {
-        return numberOfElements;
-    }
-
-    public void setNumberOfElements(int numberOfElements) {
-        this.numberOfElements = numberOfElements;
-    }
+    private PageInfo info;
 
     public Iterable getContent() {
         return content;
@@ -60,5 +12,106 @@ public class PageDto {
 
     public void setContent(Iterable content) {
         this.content = content;
+    }
+
+    public PageInfo getInfo() {
+        return info;
+    }
+
+    public void setInfo(PageInfo info) {
+        this.info = info;
+    }
+
+    public static class PageInfo {
+
+        private int totalPages;
+
+        private int number;
+
+        private boolean isFirst;
+
+        private boolean isLast;
+
+        private boolean hasNext;
+
+        private boolean hasPrevious;
+
+        private int size;
+
+        private long totalElements;
+
+        private int numberOfElements;   // <= content.length
+
+        public int getTotalPages() {
+            return totalPages;
+        }
+
+        public void setTotalPages(int totalPages) {
+            this.totalPages = totalPages;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public boolean isFirst() {
+            return isFirst;
+        }
+
+        public void setFirst(boolean first) {
+            isFirst = first;
+        }
+
+        public boolean isLast() {
+            return isLast;
+        }
+
+        public void setLast(boolean last) {
+            isLast = last;
+        }
+
+        public boolean isHasNext() {
+            return hasNext;
+        }
+
+        public void setHasNext(boolean hasNext) {
+            this.hasNext = hasNext;
+        }
+
+        public boolean isHasPrevious() {
+            return hasPrevious;
+        }
+
+        public void setHasPrevious(boolean hasPrevious) {
+            this.hasPrevious = hasPrevious;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+
+        public long getTotalElements() {
+            return totalElements;
+        }
+
+        public void setTotalElements(long totalElements) {
+            this.totalElements = totalElements;
+        }
+
+        public int getNumberOfElements() {
+            return numberOfElements;
+        }
+
+        public void setNumberOfElements(int numberOfElements) {
+            this.numberOfElements = numberOfElements;
+        }
     }
 }
