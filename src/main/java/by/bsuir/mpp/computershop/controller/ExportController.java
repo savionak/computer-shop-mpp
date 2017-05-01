86a7e1c68f61a42a2b1f629fe1ee806b38924351
Vladislav Savionok <savionak@gmail.com>
2017-05-01 19:32:32 +0300

    [Fix] ExportController mapping

--- src/main/java/by/bsuir/mpp/computershop/controller/ExportController.java
+++ src/main/java/by/bsuir/mpp/computershop/controller/ExportController.java
@@ -4,7 +4,7 @@ import by.bsuir.mpp.computershop.dto.brief.ExportBriefDto;
 import by.bsuir.mpp.computershop.dto.full.ExportFullDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("api/customer")
+@RequestMapping("api/export")
 public interface ExportController
         extends CrudController<ExportBriefDto, ExportFullDto, Long> {
 }
