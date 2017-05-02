0f9c32107699c9797e139fd8f1db53f919bec41b
LizaSandul <lizasandul97@mail.ru>
2017-05-02 23:19:16 +0300

    Add ImportServiceTest

---
+++ src/test/java/by/bsuir/mpp/computershop/utils/entity/supplier/impl/ImportEntitySupplier.java
@@ -0,0 +1,35 @@
+package by.bsuir.mpp.computershop.utils.entity.supplier.impl;
+
+import by.bsuir.mpp.computershop.entity.Import;
+import by.bsuir.mpp.computershop.utils.TestHelper;
+import by.bsuir.mpp.computershop.utils.entity.supplier.EntityLongSupplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.List;
+
+
+public class ImportEntitySupplier implements EntityLongSupplier<Import> {
+
+    @Override
+    public Import getValidEntityWithoutId() {
+        Import result =  new Import();
+        result.setId(null);
+        result.setImportDate(TestHelper.currentTimeMillis());
+        result.setCount(TestHelper.nextLong());
+        result.setPurchasePrice(TestHelper.nextLong());
+        result.setPrice(TestHelper.nextLong());
+        return result;
+    }
+
+    @Override
+    public Import getInvalidEntity() {
+        return null;
+    }
+
+    @Override
+    public Page<Import> getPage(List<Import> content) {
+        Page<Import> result = new PageImpl<>(content);
+        return result;
+    }
+}
