157fabeb6e1fef1961ff01457b9c05937174ff7d
Vladislav Savionok <savionak@gmail.com>
2017-05-03 21:28:47 +0300

    Update ErrorResponse hierarchy

---
+++ src/main/java/by/bsuir/mpp/computershop/controller/exception/dto/CustomFieldError.java
@@ -0,0 +1,27 @@
+package by.bsuir.mpp.computershop.controller.exception.dto;
+
+public class CustomFieldError {
+    private String field;
+    private String message;
+
+    public CustomFieldError(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/by/bsuir/mpp/computershop/controller/exception/dto/ErrorResponse.java
+++ src/main/java/by/bsuir/mpp/computershop/controller/exception/dto/ErrorResponse.java
@@ -1,11 +1,21 @@
 package by.bsuir.mpp.computershop.controller.exception.dto;
 
-public class ErrorResponse {
+import org.springframework.http.HttpStatus;
 
+import java.sql.Timestamp;
+
+public class ErrorResponse {
+    private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    private HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
     private String message;
 
     public ErrorResponse(String message) {
         this.message = message;
+    }
+
+    public ErrorResponse(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
     }
 
     public String getMessage() {
@@ -14,5 +24,17 @@ public class ErrorResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
     }
 }
