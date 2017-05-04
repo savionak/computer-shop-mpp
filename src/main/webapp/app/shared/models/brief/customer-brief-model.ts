6200f0a3d43fee438170efff773142661d9a4915
LizaSandul <lizasandul97@mail.ru>
2017-05-04 13:26:35 +0300

    Add Customer models

---
+++ src/main/webapp/app/shared/models/brief/customer-brief-model.ts
@@ -0,0 +1,8 @@
+import {BaseModel} from "../../base-model";
+
+export class CustomerBriefModel extends BaseModel {
+    name: string;
+    description: string;
+    ordersCount:number;
+
+}
---
+++ src/main/webapp/app/shared/models/full/customer-model.ts
@@ -0,0 +1,12 @@
+import {BaseModel} from "../../base-model";
+
+export class CustomerModel extends BaseModel {
+    name: string;
+    description: string;
+    removed: boolean;
+    ordersCount:number;
+
+    public static empty(): CustomerModel {
+        return {id: null, name: "", description: null, removed: false,ordersCount:null}
+    }
+}
