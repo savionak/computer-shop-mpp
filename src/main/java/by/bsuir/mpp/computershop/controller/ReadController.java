7282989f2a6f99fa6556f3933a90892a1e76185c
Vladislav Savionok <savionak@gmail.com>
2017-05-06 15:31:26 +0300

    Update Controller interfaces
    * split CrudController -> add ReadController
    * split AbstractCrudController -> add AbstractReadController

---
+++ src/main/java/by/bsuir/mpp/computershop/controller/ReadController.java
@@ -0,0 +1,20 @@
+package by.bsuir.mpp.computershop.controller;
+
+import by.bsuir.mpp.computershop.controller.exception.ControllerException;
+import by.bsuir.mpp.computershop.dto.PageDto;
+import by.bsuir.mpp.computershop.dto.full.BaseFullDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.Serializable;
+
+public interface ReadController<F extends BaseFullDto<ID>, ID extends Serializable> {
+
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    F getById(@PathVariable ID id) throws ControllerException;
+
+    @RequestMapping(method = RequestMethod.GET)
+    PageDto getAll(Pageable pageable) throws ControllerException;
+}
