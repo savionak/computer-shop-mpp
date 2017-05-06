ece510488cf4660000b50299ae295f01eb115e28
Vladislav Savionok <savionak@gmail.com>
2017-05-06 20:37:44 +0300

    Update ComponentModel stack

--- src/main/java/by/bsuir/mpp/computershop/repository/ComponentModelRepository.java
+++ src/main/java/by/bsuir/mpp/computershop/repository/ComponentModelRepository.java
@@ -1,9 +1,28 @@
 package by.bsuir.mpp.computershop.repository;
 
 import by.bsuir.mpp.computershop.entity.ComponentModel;
-import by.bsuir.mpp.computershop.entity.ComponentType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ComponentModelRepository extends PagingAndSortingRepository<ComponentModel, Long> {
-    Iterable<ComponentModel> getComponentModelsByType(ComponentType type);
+
+    @Override
+    @Transactional
+    @Modifying
+    @Query(value = "CALL remove_model(:id);", nativeQuery = true)
+    void delete(@Param("id") Long id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "CALL restore_model(:id);", nativeQuery = true)
+    void restore(@Param("id") Long id);
+
+    Page<ComponentModel> findAllByRemovedIsFalse(Pageable pageable);
+
+    Page<ComponentModel> findAllByRemovedIsTrue(Pageable pageable);
 }
--- src/main/java/by/bsuir/mpp/computershop/service/impl/ComponentModelServiceImpl.java
+++ src/main/java/by/bsuir/mpp/computershop/service/impl/ComponentModelServiceImpl.java
@@ -3,14 +3,37 @@ package by.bsuir.mpp.computershop.service.impl;
 import by.bsuir.mpp.computershop.entity.ComponentModel;
 import by.bsuir.mpp.computershop.repository.ComponentModelRepository;
 import by.bsuir.mpp.computershop.service.ComponentModelService;
+import by.bsuir.mpp.computershop.service.exception.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import static by.bsuir.mpp.computershop.service.exception.wrapper.RepositoryCallWrapper.wrapRepositoryCall;
 
 @Service
 public class ComponentModelServiceImpl extends AbstractCrudService<ComponentModel, Long> implements ComponentModelService {
 
+    private final ComponentModelRepository modelRepository;
+
     @Autowired
     public ComponentModelServiceImpl(ComponentModelRepository modelRepository) {
         super(modelRepository);
+        this.modelRepository = modelRepository;
+    }
+
+    @Override
+    public Page<ComponentModel> getAll(Pageable pageable) throws ServiceException {
+        return wrapRepositoryCall(() -> modelRepository.findAllByRemovedIsFalse(pageable));
+    }
+
+    @Override
+    public Page<ComponentModel> getRemoved(Pageable pageable) throws ServiceException {
+        return wrapRepositoryCall(() -> modelRepository.findAllByRemovedIsTrue(pageable));
+    }
+
+    @Override
+    public void restore(Long id) throws ServiceException {
+        wrapRepositoryCall(() -> modelRepository.restore(id));
     }
 }
