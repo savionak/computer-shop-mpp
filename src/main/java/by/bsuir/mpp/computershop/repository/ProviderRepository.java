55a71670b2afbde1ae69c1cb251a58ff3193af33
Vladislav Savionok <savionak@gmail.com>
2017-05-06 17:33:09 +0300

    Update ProviderRepository
    * override delete()

--- src/main/java/by/bsuir/mpp/computershop/repository/ProviderRepository.java
+++ src/main/java/by/bsuir/mpp/computershop/repository/ProviderRepository.java
@@ -3,9 +3,17 @@ package by.bsuir.mpp.computershop.repository;
 import by.bsuir.mpp.computershop.entity.Provider;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProviderRepository extends PagingAndSortingRepository<Provider, Long> {
+
+    @Override
+    @Modifying
+    @Query(value = "CALL remove_provider(:id);", nativeQuery = true)
+    void delete(@Param("id") Long id);
 
     Page<Provider> findAllByRemovedIsFalse(Pageable pageable);
 
