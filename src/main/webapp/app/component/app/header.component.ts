f54aaeac76855d202105554b1309f77cbc3da96c
LizaSandul <lizasandul97@mail.ru>
2017-05-07 00:57:03 +0300

    refactor style of pages

--- src/main/webapp/app/component/app/header.component.ts
+++ src/main/webapp/app/component/app/header.component.ts
@@ -3,8 +3,30 @@ import { Component} from '@angular/core';
 @Component({
     selector: 'header',
     template: `
-<div class="header">
-    <span class="header_title"> Computer Store    </span>
-    </div>`
+
+		<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar">
+					</span> <span class="icon-bar">
+					</span> <span class="icon-bar">
+					</span>
+				 </button>
+			<div class="navbar-header">
+				<a class="navbar-brand" routerLink=""><img src="app/styles/logo.png" alt="MPP"></a>
+			</div>
+
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li ><a routerLink="provider">Provider</a></li>
+					<li><a routerLink="component">ComponentType</a></li>
+					<li><a class="btn" routerLink="login">Exit</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+`
 })
 export class HeaderComponent { }
