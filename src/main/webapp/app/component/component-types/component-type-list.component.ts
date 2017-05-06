5b3530a83ffa5fed0cf991c691bc32f232477c73
LizaSandul <lizasandul97@mail.ru>
2017-05-07 00:57:31 +0300

    Add table to ComponentTypes

--- src/main/webapp/app/component/component-types/component-type-list.component.ts
+++ src/main/webapp/app/component/component-types/component-type-list.component.ts
@@ -1,17 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {AbstractListComponent} from "../app/list.component";
 import {ComponentTypeService} from "../../service/component-type.service";
 import {ComponentTypeModel} from "../../model/full/component-type-model";
 import {ComponentTypeBriefModel} from "../../model/brief/component-type-brief-model";
-
 @Component({
+
     selector: 'comp-type-list',
     templateUrl: './component-type-list.component.html',
     providers: [    // local provider -- created for each component <= replace with global
         ComponentTypeService
     ]
 })
-export class ComponentTypesListComponent extends AbstractListComponent<ComponentTypeModel, ComponentTypeBriefModel>{
+export class ComponentTypesListComponent extends AbstractListComponent<ComponentTypeModel, ComponentTypeBriefModel> {
 
     constructor(private componentTypeService: ComponentTypeService) {
         super(componentTypeService);
