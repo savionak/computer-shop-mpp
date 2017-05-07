1bbac2ed4e9105f1cdba0de24d8e1763c8a4519e
Vladislav Savionok <savionak@gmail.com>
2017-05-07 20:40:33 +0300

    [Fix] LogoutController NullPointerException

--- src/main/java/by/bsuir/mpp/computershop/controller/LogoutController.java
+++ src/main/java/by/bsuir/mpp/computershop/controller/LogoutController.java
@@ -1,5 +1,6 @@
 package by.bsuir.mpp.computershop.controller;
 
+import by.bsuir.mpp.computershop.controller.exception.ControllerException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public interface LogoutController {
 
     @RequestMapping(path = "/oauth/revoke_token", method = RequestMethod.POST)
-    void revokeToken(OAuth2Authentication auth);
+    void revokeToken(OAuth2Authentication auth) throws ControllerException;
 
 }
