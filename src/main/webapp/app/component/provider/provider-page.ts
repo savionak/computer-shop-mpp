c88a9ee9957c3bc82372dd77d3465180b056b528
Vladislav Savionok <savionak@gmail.com>
2017-05-10 02:49:01 +0300

    Connect two lists via ViewChild on page
    + small fix

--- src/main/webapp/app/component/provider/provider-page.ts
+++ src/main/webapp/app/component/provider/provider-page.ts
@@ -1,6 +1,8 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 
 import {ProviderService} from "../../service/provider.service";
+import {ProviderListComponent} from "./provider-list.component";
+import {ProviderRemovedListComponent} from "./provider-removed-list.component";
 
 
 @Component({
@@ -11,16 +13,19 @@ export class ProviderPage {
     private service: ProviderService;
     protected error: string;
 
+    @ViewChild(ProviderListComponent) list: ProviderListComponent;
+    @ViewChild(ProviderRemovedListComponent) removedList: ProviderRemovedListComponent;
+
     constructor(service: ProviderService) {
         this.service = service;
     }
 
     onDeleteDone() {
-
+        this.removedList.onRefresh();
     }
 
     onRestoreDone() {
-
+        this.list.onRefresh();
     }
 
     onError(error: string) {
--- src/main/webapp/app/component/provider/provider-removed-list.component.ts
+++ src/main/webapp/app/component/provider/provider-removed-list.component.ts
@@ -2,13 +2,14 @@ import {Component} from "@angular/core";
 import {ProviderService} from "../../service/provider.service";
 import {ProviderBriefModel} from "../../model/brief/provider-brief-model";
 import {RemovedListComponent} from "../base/removed-list.component";
+import {ProviderModel} from "../../model/full/provider-model";
 
 
 @Component({
     selector: 'provider-removed-list',
     templateUrl: './provider-removed-list.component.html'
 })
-export class ProviderRemovedListComponent extends RemovedListComponent<ProviderBriefModel, ProviderBriefModel> {
+export class ProviderRemovedListComponent extends RemovedListComponent<ProviderModel, ProviderBriefModel> {
     constructor(service: ProviderService) {
         super(service);
     }
