5c472a29f19682fec5b3c5be77456361efa62305
Vladislav Savionok <savionak@gmail.com>
2017-05-09 19:27:12 +0300

    [Angular] Provider Edit and Add as one component

--- src/main/webapp/app/component/provider/provider-list.component.ts
+++ src/main/webapp/app/component/provider/provider-list.component.ts
@@ -1,7 +1,6 @@
 import {Component} from "@angular/core";
 
 import {ProviderService} from "../../service/provider.service";
-import {AbstractListComponent} from "../app/list.component";
 import {ProviderModel} from "../../model/full/provider-model";
 import {ProviderBriefModel} from "../../model/brief/provider-brief-model";
 
@@ -10,14 +9,134 @@ import {ProviderBriefModel} from "../../model/brief/provider-brief-model";
     selector: 'provider-list',
     templateUrl: './provider-list.component.html'
 })
-export class ProviderListComponent extends AbstractListComponent<ProviderModel, ProviderBriefModel> {
+export class ProviderListComponent {
+    private service: ProviderService;
 
-    constructor(private componentTypeService: ProviderService) {
-        super(componentTypeService);
+    protected modelsList: ProviderBriefModel[];
+    protected error: string;
+
+    protected model: ProviderModel;
+    protected isEditing: boolean;
+
+    ngOnInit() {
+        this.refreshList();
+    }
+
+    constructor(service: ProviderService) {
+        this.service = service;
     }
 
     getEmptyModel(): ProviderModel {
         return ProviderModel.empty();
     }
 
+    refreshList() {
+        this.service.getList()
+            .subscribe(
+                (page) => {
+                    this.modelsList = page.content
+                },
+                (error) => {
+                    // TODO: show popup
+                    this.error = error;
+                    alert(error);
+                }
+            )
+    }
+
+    onRefresh(): void {
+        this.refreshList();
+    }
+
+    onDelete(model: ProviderBriefModel): void {
+        this.service.remove(model.id)
+            .subscribe(
+                (res) => {
+                    this.refreshList()
+                },
+                (error) => {
+                    // TODO: show popup
+                    this.error = error;
+                    alert(error);
+                }
+            );
+    }
+
+    onAdd(): void {
+        this.isEditing = false;
+        this.model = this.getEmptyModel();
+    }
+
+    onEdit(model: ProviderBriefModel): void {
+        this.isEditing = true;
+
+        this.service.get(model.id)
+            .subscribe(
+                (res) => {
+                    this.model = res;
+                },
+                (error) => {
+                    // TODO: show popup
+                    this.error = error;
+                    alert(error);
+                }
+            );
+    }
+
+    onSave(): void {
+        this.service.update(this.model.id, this.model)
+            .subscribe(
+                (res) => {
+                    this.endEditing();
+                    this.refreshList();
+                },
+                (error) => {
+                    // TODO: show popup
+                    this.error = error;
+                    alert(error);
+                }
+            );
+    }
+
+    onAction(): void {
+        if (this.isEditing) {
+            this.service.update(this.model.id, this.model)
+                .subscribe(
+                    (res) => {
+                        this.endEditing();
+                        this.refreshList();
+                    },
+                    (error) => {
+                        // TODO: show popup
+                        this.error = error;
+                        alert(error);
+                    }
+                );
+        } else {
+            this.service.add(this.model)
+                .subscribe(
+                    (res) => {
+                        this.endEditing();
+                        this.refreshList();
+                    },
+                    (error) => {
+                        // TODO: show popup
+                        this.error = error;
+                        alert(error);
+                    }
+                );
+        }
+    }
+
+    onCancel(): void {
+        this.endEditing();
+    }
+
+    endEditing(): void {
+        this.model = null;
+    }
+
+    onCloseError() {
+        this.error = null;
+    }
 }
