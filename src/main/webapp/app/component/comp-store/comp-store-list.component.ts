bd1c791b11da28b7e6a8c5fad3d8457092234b18
LizaSandul <lizasandul97@mail.ru>
2017-05-10 13:16:25 +0300

    [Fix] ComponentStoreList

--- src/main/webapp/app/component/comp-store/comp-store-list.component.ts
+++ src/main/webapp/app/component/comp-store/comp-store-list.component.ts
@@ -19,4 +19,8 @@ export class ComponentStoreListComponent extends ReadOnlyListComponent<Component
     protected getEmptyModel(): ComponentStoreModel {
         return ComponentStoreModel.empty();
     }
+
+    onChange(model: ComponentStoreBriefModel) {
+
+    }
 }
