3a30fab63246f19600bc2ffa8c5554aee6d552b5
Vladislav Savionok <savionak@gmail.com>
2017-05-10 08:22:23 +0300

    [Angular] Update ComponentModelBriefModel

--- src/main/webapp/app/model/brief/component-model-brief-model.ts
+++ src/main/webapp/app/model/brief/component-model-brief-model.ts
@@ -2,6 +2,8 @@ import {BaseModel} from "../base-model";
 
 
 export class ComponentModelBriefModel extends BaseModel {
+    typeId: number;
+    typeName: number;
     name: string;
     description: string;
 }
