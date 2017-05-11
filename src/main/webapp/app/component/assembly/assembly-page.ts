8fb07bb9658ab43c58e7b3ef8ee850d04ab4221b
Vladislav Savionok <savionak@gmail.com>
2017-05-11 16:20:27 +0300

    [Angular] Embed OrderEdit into AssemblyList
    + small refactor

--- src/main/webapp/app/component/assembly/assembly-page.ts
+++ src/main/webapp/app/component/assembly/assembly-page.ts
@@ -3,6 +3,8 @@ import {Component, OnInit, ViewChild} from "@angular/core";
 import {AssemblyListComponent} from "./assembly-list.component";
 import {AssemblyService} from "../../service/assembly.service";
 import {ActivatedRoute} from "@angular/router";
+import {OrderModel} from "../../model/full/order-model";
+import {OrderService} from "../../service/order.service";
 
 
 @Component({
@@ -14,19 +16,24 @@ export class AssemblyPage implements OnInit {
     protected error: string;
 
     protected orderId: number;
+    protected order: OrderModel;
+
     protected isReadOnly: boolean;
 
     @ViewChild(AssemblyListComponent) list: AssemblyListComponent;
 
-    constructor(service: AssemblyService, private route: ActivatedRoute) {
+    constructor(service: AssemblyService, private orderService: OrderService, private route: ActivatedRoute) {
         this.service = service;
     }
 
     ngOnInit(): void {
         let urlSegments = this.route.snapshot.url;
         this.isReadOnly = urlSegments[1].toString() !== 'edit';
-
         this.orderId = +this.route.snapshot.params['id'];
+        this.orderService.get(this.orderId).subscribe(
+            model => this.order = model,
+            error => this.error = error
+        );
     }
 
     onDeleteDone() {
