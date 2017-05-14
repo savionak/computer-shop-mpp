e92595ce1606038751ed03e4775ee6d0defeb922
Vladislav Savionok <savionak@gmail.com>
2017-05-14 22:34:35 +0300

    [Angular] Simplify NotFoundPage

--- src/main/webapp/app/component/app/not-found-page.ts
+++ src/main/webapp/app/component/app/not-found-page.ts
@@ -1,30 +1,9 @@
-import {Component, OnDestroy, OnInit} from "@angular/core";
-import {NavigationEnd, Router} from "@angular/router";
-import {Subscription} from "rxjs/Subscription";
+import {Component} from "@angular/core";
 
 @Component({
     selector: 'not-found',
     templateUrl: './not-found-page.html'
 })
-export class NotFoundPage implements OnInit, OnDestroy {
-    private path: string = null;
-    private sub: Subscription;
+export class NotFoundPage {
 
-    constructor(private router: Router) {
-
-    }
-
-    ngOnInit(): void {
-        this.sub = this.router.events.subscribe(
-            event => {
-                if (event instanceof NavigationEnd) {
-                    this.path = event.url;
-                }
-            }
-        );
-    }
-
-    ngOnDestroy(): void {
-        this.sub.unsubscribe();
-    }
 }
