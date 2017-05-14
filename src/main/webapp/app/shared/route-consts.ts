444178e6800bba659209c237ce0835831efa42ab
Vladislav Savionok <savionak@gmail.com>
2017-05-14 18:20:56 +0300

    [Angular] Introduce role-based routes map

--- src/main/webapp/app/shared/route-consts.ts
+++ src/main/webapp/app/shared/route-consts.ts
@@ -1,3 +1,5 @@
+import {Role} from "../model/full/user-auth-model";
+
 export const LOGIN = 'login';
 
 // Main routes
@@ -15,3 +17,49 @@ export const ASSEMBLY = 'asm';
 export const ACCESS = 'access';
 export const EDIT = 'edit';
 export const VIEW = 'view';
+
+
+const DIRECTOR_ROUTES = {
+    'default': ORDER + '/' + VIEW,
+    'items': [
+        {'Orders': ORDER + '/' + VIEW},
+        {'Customers': CUSTOMER + '/' + VIEW},
+        {'Store': STORE + '/' + VIEW},
+        {'Imports': IMPORT + '/' + VIEW},
+        {'Providers': PROVIDER + '/' + EDIT}
+    ]
+};
+
+const MANAGER_ROUTES = {
+    'default': ORDER + '/' + EDIT,
+    'items': [
+        {'Orders': ORDER + '/' + EDIT},
+        {'Customers': CUSTOMER + '/' + EDIT},
+        {'Store': STORE + '/' + EDIT},
+        {'Imports': IMPORT + '/' + EDIT},
+        {'Models': MODEL + '/' + EDIT},
+        {'Types': TYPE + '/' + VIEW},
+        {'Providers': PROVIDER + '/' + VIEW}
+    ]
+};
+
+const ADMIN_ROUTES = {
+    'default': USER + '/' + EDIT,
+    'items': [
+        {'Users': USER + '/' + EDIT},
+        {'Orders': ORDER + '/' + EDIT},
+        {'Customers': CUSTOMER + '/' + EDIT},
+        {'Store': STORE + '/' + EDIT},
+        {'Imports': IMPORT + '/' + EDIT},
+        {'Models': MODEL + '/' + EDIT},
+        {'Types': TYPE + '/' + EDIT},
+        {'Providers': PROVIDER + '/' + EDIT}
+    ]
+};
+
+
+export const ROUTES = {};
+
+ROUTES[Role.DIRECTOR] = DIRECTOR_ROUTES;
+ROUTES[Role.MANAGER] = MANAGER_ROUTES;
+ROUTES[Role.ADMIN] = ADMIN_ROUTES;
