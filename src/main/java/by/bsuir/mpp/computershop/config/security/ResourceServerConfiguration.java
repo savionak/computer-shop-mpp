a9265c87982f0ba73179663cb1c8ce7a4fb08220
Vladislav Savionok <savionak@gmail.com>
2017-05-14 20:36:06 +0300

    [Server] Update Security config

--- src/main/java/by/bsuir/mpp/computershop/config/security/ResourceServerConfiguration.java
+++ src/main/java/by/bsuir/mpp/computershop/config/security/ResourceServerConfiguration.java
@@ -4,6 +4,7 @@ import by.bsuir.mpp.computershop.entity.UserAuth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -38,11 +39,27 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        String DIRECTOR = UserAuth.Role.DIRECTOR.name();
+        String MANAGER = UserAuth.Role.MANAGER.name();
+        String ADMIN = UserAuth.Role.ADMIN.name();
+
         http.
                 authorizeRequests()
-                    .antMatchers("/").anonymous()
-                // TODO: configure access for roles
-                .antMatchers("/api/**").hasAuthority(UserAuth.Role.ADMIN.name())
-                .anyRequest().permitAll();
+                .antMatchers(HttpMethod.GET, "/").anonymous()
+                .antMatchers(HttpMethod.POST, "/oauth/revoke_token").authenticated()
+
+                .antMatchers(HttpMethod.GET, "/api/**").authenticated()
+
+                .antMatchers("/api/provider/**").hasAnyAuthority(DIRECTOR, ADMIN)
+
+                .antMatchers("/api/order/**",
+                        "/api/customer/**",
+                        "/api/component/store/**",
+                        "/api/assembly",
+                        "/api/imports",
+                        "/api/component/model").hasAnyAuthority(MANAGER, ADMIN)
+
+                .antMatchers("/api/user/**", "/api/component/type/**").hasAuthority(ADMIN)
+        ;
     }
 }
