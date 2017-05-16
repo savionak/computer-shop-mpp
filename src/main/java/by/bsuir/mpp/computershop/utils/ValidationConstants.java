330d108fb06d59269bc0816e53207c7e4a35550c
Vladislav Savionok <savionak@gmail.com>
2017-05-16 21:43:02 +0300

    Rewrite server error messages

--- src/main/java/by/bsuir/mpp/computershop/utils/ValidationConstants.java
+++ src/main/java/by/bsuir/mpp/computershop/utils/ValidationConstants.java
@@ -4,9 +4,9 @@ public final class ValidationConstants {
     public static final String NON_EMPTY_STRING_REGEX = "^(?!\\s*$).+";
     public static final String EMAIL_REGEX = "^(?!\\s*$).+@(?!\\s*$).+";
 
-    public static final String CANNOT_BE_NULL_MESSAGE = "cannot be NULL";
-    public static final String CANNOT_BE_EMPTY_MESSAGE = "cannot be empty";
-    public static final String CANNOT_BE_NEGATIVE_MESSAGE = "cannot be negative";
+    public static final String CANNOT_BE_NULL_MESSAGE = "не может быть пустым";
+    public static final String CANNOT_BE_EMPTY_MESSAGE = "не может быть пустым";
+    public static final String CANNOT_BE_NEGATIVE_MESSAGE = "не может быть отрицательным";
 
-    public static final String INVALID_VALUE_MESSAGE = "invalid value";
+    public static final String INVALID_VALUE_MESSAGE = "имеет неверное значение";
 }
