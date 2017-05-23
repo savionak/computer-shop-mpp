df5fc043d2b4e0e7961ed45b60d1527181d9b02b
Vladislav Savionok <savionak@gmail.com>
2017-05-23 22:59:22 +0300

    Add DocumentController

--- src/main/java/by/bsuir/mpp/computershop/controller/exception/wrapper/WrappedServiceFunctions.java
+++ src/main/java/by/bsuir/mpp/computershop/controller/exception/wrapper/WrappedServiceFunctions.java
@@ -1,6 +1,9 @@
 package by.bsuir.mpp.computershop.controller.exception.wrapper;
 
 import by.bsuir.mpp.computershop.service.exception.ServiceException;
+import com.itextpdf.text.DocumentException;
+
+import java.io.IOException;
 
 public class WrappedServiceFunctions {
 
@@ -12,5 +15,10 @@ public class WrappedServiceFunctions {
     @FunctionalInterface
     public interface VoidServiceFunction {
         void call() throws ServiceException;
+    }
+
+    @FunctionalInterface
+    public interface DocumentGeneratorCall {
+        void call() throws DocumentException, IOException;
     }
 }
