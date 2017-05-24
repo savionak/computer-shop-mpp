0d5ca812c26dda3cae4ec141430d96a28bc8003f
Vladislav Savionok <savionak@gmail.com>
2017-05-25 00:35:51 +0300

    Update UsersProvider

--- src/main/java/by/bsuir/mpp/computershop/document/model/provider/impl/UsersProvider.java
+++ src/main/java/by/bsuir/mpp/computershop/document/model/provider/impl/UsersProvider.java
@@ -1,7 +1,6 @@
 package by.bsuir.mpp.computershop.document.model.provider.impl;
 
 import by.bsuir.mpp.computershop.document.model.provider.ContentProvider;
-import by.bsuir.mpp.computershop.entity.ComponentStore;
 import by.bsuir.mpp.computershop.entity.UserAuth;
 import org.springframework.data.util.Pair;
 
@@ -11,16 +10,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 
-public class UserProvider implements ContentProvider<UserAuth, UserAuth> {
+public class UsersProvider implements ContentProvider<UserAuth, UserAuth> {
     @Override
     public String createFilename(UserAuth userAuth) {
-        return "user_auth_state";
+        return "users_list";
     }
 
     @Override
     public String createTitle(UserAuth userAuth) {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        return String.format("User Auth State for %s", dateFormat.format(new Date()));
+        return String.format("Users list State for %s", dateFormat.format(new Date()));
     }
 
     @Override
@@ -30,22 +29,23 @@ public class UserProvider implements ContentProvider<UserAuth, UserAuth> {
 
     @Override
     public List<String> getHeaders() {
-        return Arrays.asList("Type", "Model", "Price", "Count");
+        return Arrays.asList("Email", "Role", "First Name", "Last Name");
     }
 
     @Override
-    public String createTableTitle(UserAuth userAuth, Collection<UserAuth> courseFeedbacks) {
-        return "Component store state";
+    public String createTableTitle(UserAuth userAuth, Collection<UserAuth> users) {
+        return "Active users list";
     }
 
     @Override
     public List<List<String>> createRows(Collection<UserAuth> userAuths) {
         return userAuths.stream()
                 .map(userAuthItem -> Arrays.asList(
-                        userAuthItem.getModel().getType().getName(),
-                        userAuthItem.getModel().getName(),
-                        userAuthItem.getPrice().toString(),
-                        userAuthItem.getCount().toString()))
+                        userAuthItem.getEmail(),
+                        userAuthItem.getRole().toString(),
+                        userAuthItem.getUserInfo().getFirstName(),
+                        userAuthItem.getUserInfo().getLastName())
+                )
                 .collect(Collectors.toList());
     }
 }
