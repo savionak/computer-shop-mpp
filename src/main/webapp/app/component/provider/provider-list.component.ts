72f72f01f4f0b144dff081d0935b0c5e23a91a3f
LizaSandul <lizasandul97@mail.ru>
2017-05-25 07:59:26 +0300

    refactor

--- src/main/webapp/app/component/provider/provider-list.component.ts
+++ src/main/webapp/app/component/provider/provider-list.component.ts
@@ -4,6 +4,7 @@ import {ListComponent} from "../base/list.component";
 import {ProviderService} from "../../service/provider.service";
 import {ProviderModel} from "../../model/full/provider-model";
 import {ProviderBriefModel} from "../../model/brief/provider-brief-model";
+import {DocumentService, DocumentType} from "../../shared/document.service";
 
 
 @Component({
@@ -11,11 +12,18 @@ import {ProviderBriefModel} from "../../model/brief/provider-brief-model";
     templateUrl: './provider-list.component.html'
 })
 export class ProviderListComponent extends ListComponent<ProviderModel, ProviderBriefModel> {
-    constructor(service: ProviderService) {
+    constructor(service: ProviderService,
+                private documentService: DocumentService) {
         super(service);
     }
 
     protected getEmptyModel(): ProviderModel {
         return ProviderModel.empty();
+    }
+
+
+    onGetAllPageID(id,documentType: DocumentType) {
+        // alert("here");
+        this.documentService.getImportState(documentType);
     }
 }
--- src/main/webapp/app/shared/document.service.ts
+++ src/main/webapp/app/shared/document.service.ts
@@ -13,11 +13,35 @@ export class DocumentService {
 
     private static readonly DOCS_URL = "api/documents";
     private static readonly STORE_URL = "/store";
+    private static readonly USER_URL = "/user";
+    private static readonly PROVIDER_URL = "/provider";
+    private static readonly PROVIDER_ID_URL = "/provider/{id}";
+    private static readonly CUSTOMER_URL = "/customer";
 
     // отчет о текущем состоянии склада
     public getStoreState(documentType: DocumentType) {
         console.log("type:" + documentType);
         this.http.getDocument(DocumentService.DOCS_URL + DocumentService.STORE_URL, documentType);
+    }
+
+    public getCustomerState(documentType: DocumentType) {
+        console.log("type:" + documentType);
+        this.http.getDocument(DocumentService.DOCS_URL + DocumentService.CUSTOMER_URL, documentType);
+    }
+
+    public getImportState(,documentType: DocumentType) {
+        console.log("type:" + documentType);
+        this.http.getDocument(DocumentService.DOCS_URL + DocumentService.PROVIDER_ID_URL, documentType);
+    }
+
+    public getUserState(documentType: DocumentType) {
+        console.log("type:" + documentType);
+        this.http.getDocument(DocumentService.DOCS_URL + DocumentService.USER_URL, documentType);
+    }
+
+    public getProviderState(documentType: DocumentType) {
+        console.log("type:" + documentType);
+        this.http.getDocument(DocumentService.DOCS_URL + DocumentService.PROVIDER_URL, documentType);
     }
 
 }
